3dd55897864d3e5f47b53daf3ba860c42e41d563
Neo Alienson <neo@01man.com>
2025-06-11 22:36:00 +0800

    init

---
+++ tests/test_tools.py
@@ -0,0 +1,15 @@
+import unittest
+from tests.test_tool_business_reg import TestBusinessReturns
+from tests.test_tool_passenger_traffic import TestPassengerTraffic
+
+def suite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromTestCase(TestBusinessReturns))
+    suite.addTests(loader.loadTestsFromTestCase(TestPassengerTraffic))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=2)
+    test_suite = suite()
+    runner.run(test_suite)
